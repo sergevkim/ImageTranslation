c839f2c4f28c9bcc21c91f500612ab3c8935354c
sergevkim <sergevkim@gmail.com>
2020-12-14 21:01:37 +0300

    facades only

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -2,11 +2,11 @@
 
 mkdir data
 cd data
-wget http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/cityscapes.tar.gz
+#wget http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/cityscapes.tar.gz
 wget http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/facades.tar.gz
-tar -xvzf cityscapes.tar.gz
+#tar -xvzf cityscapes.tar.gz
 tar -xvzf facades.tar.gz
-rm cityscapes.tar.gz
+#rm cityscapes.tar.gz
 rm facades.tar.gz
 cd ..
 
